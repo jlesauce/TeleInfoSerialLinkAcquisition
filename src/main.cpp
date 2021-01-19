392474a02ce867355401a695dc57973e11868d10
Julien LE SAUCE <julien.lesauce@gmail.com>
2021-01-19 14:51:31 +0100

    Update linker settings to link wiringPi

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-#include <cstdio>
-#include <termios.h>
 #include <unistd.h>
-#include <cstring>
-#include <cerrno>
 #include <wiringPi.h>
 #include <wiringSerial.h>
-#include <vector>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    char *levelSensorPort = "/dev/ttyUSB0"; //Serial Device Address
+    string serialDeviceAddress = "/dev/ttyAMA0";
 
-    int levelSensor = serialOpen(levelSensorPort, 19200);
+    int levelSensor = serialOpen(serialDeviceAddress.c_str(), 19200);
     wiringPiSetup();
     serialPuts(levelSensor, "DP"); //Send command to the serial device
 
-    while (1) {
+    while (true) {
         char buffer[100];
         ssize_t length = read(levelSensor, &buffer, sizeof(buffer));
         if (length == -1) {
